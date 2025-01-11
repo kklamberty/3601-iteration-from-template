d7f6e737b5f4d4d7ecf9ad72e6afa6de1256db42
K.K. Lamberty <lamberty@morris.umn.edu>
2025-01-11 13:55:50 -0600

    Initialize add-timeout-to-database-seeding

---
+++ client/cypress/e2e/company-list.cy.ts
@@ -0,0 +1,37 @@
+import { CompanyListPage } from '../support/company-list.po';
+
+const page = new CompanyListPage();
+
+describe('Company list', () => {
+
+  before(() => {
+      // Wait up to 30 seconds for the seeding to complete.
+      cy.task('seed:database', null, { timeout: 30000 } );
+  });
+
+  beforeEach(() => {
+    page.navigateTo();
+  });
+
+  it('Should have the correct title', () => {
+    page.getUserTitle().should('have.text', 'Companies');
+  });
+
+  // There are 10 users in the database, two of which work
+  // for OHMNET, so there are 9 different companies.
+  it('Should show 9 companies in the list', () => {
+    page.getCompanyCards().should('have.length', 9);
+  });
+
+  it('Should contain a company with the name "OHMNET"', () => {
+    page.getCompanyCardByName('OHMNET').should('exist');
+  });
+
+  it('Should contain a company with the name "OHMNET" and 2 users', () => {
+    page.getCompanyCardUserNames('OHMNET').should('have.length', 2);
+  });
+
+  it('Should contain a company with the name "VINCH" and 1 user', () => {
+    page.getCompanyCardUserNames('VINCH').should('have.length', 1);
+  });
+});
