052ecaea3f55b74f84917ae4ef9a948e4d9ba69d
K.K. Lamberty <lamberty@morris.umn.edu>
2025-01-11 13:55:41 -0600

    Initialize 875-introduce-multi-line-strings-in-the-

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './users/user-list.component';
+import { UserProfileComponent } from './users/user-profile.component';
+import { AddUserComponent } from './users/add-user.component';
+
+// Note that the 'users/new' route needs to come before 'users/:id'.
+// If 'users/:id' came first, it would accidentally catch requests to
+// 'users/new'; the router would just think that the string 'new' is a user ID.
+const routes: Routes = [
+  {path: '', component: HomeComponent, title: 'Home'},
+  {path: 'users', component: UserListComponent, title: 'Users'},
+  {path: 'users/new', component: AddUserComponent, title: 'Add User'},
+  {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
